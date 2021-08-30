a0faedd63260bd86f9c38b22ecf6cf15e0e6fb51
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-08-30 19:07:28 +0530

    Added Dark mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light');  // Weather dark mode is enable or not.
+  const toggleMode = () =>{
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#051f39";
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
   return (
     <>
-    <Navbar title="TextUtils" about="About" />
-    <TextForm heading="Enter a text to analyze:"/>
+    <Navbar title="TextUtils" about="About" mode={mode} toggleMode={toggleMode} />
+    <TextForm heading="Enter a text to analyze below" mode={mode}/>
+    {/* <About /> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
+
+    const [text, setText] = useState("")
+
     const handleUpClick =() => {
         // console.log("Uppercase was clicked");
         let newText = text.toUpperCase();
@@ -23,9 +26,20 @@ export default function TextForm(props) {
         let str1 = wor.join(" ");
         setText(str1)
     }
-    
+
+    const handleCopy =() => {
+        var text =document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
     const handleClearClick =() => {
         setText("")
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
     }
 
     const handleOnChange = (event) => {
@@ -33,28 +47,30 @@ export default function TextForm(props) {
         setText(event.target.value)
     }
 
-    const [text, setText] = useState("")
-
     
     return (
         <>
-        <div className="container mb-3">
+        <div className="container mb-3 " style={{color:props.mode ==="dark"? "rgb(252 240 240)" :"#212529"}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <label htmlFor="myBox" className="form-label"></label>
-                <textarea className="form-control" id="myBox" rows="10" placeholder="Enter text here" value={text} onChange={handleOnChange}></textarea>
+                <label htmlFor="myBox" className="form-label "></label>
+                <textarea className="form-control " id="myBox" rows="10" placeholder="Enter text here" style={{backgroundColor: props.mode ==="light"? "white" :"#051f39", color:props.mode ==="dark"? "rgb(252 240 240)" :"black"}} value={text} onChange={handleOnChange}></textarea>
             </div>
-            <div className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</div>
-            <div className="btn btn-success mx-3" onClick={handleLowClick}>Convert to Lowercase</div>
-            <div className="btn btn-danger mx-3" onClick={handleCewClick}>Capitalize each word</div>
-            <div className="btn btn-danger mx-3" onClick={handleClearClick}>Clear Text</div>
+            <div className="btn btn-info" onClick={handleUpClick}>Convert to Uppercase</div>
+            <div className="btn btn-info mx-3" onClick={handleLowClick}>Convert to Lowercase</div>
+            <div className="btn btn-info mx-3" onClick={handleCewClick}>Capitalize each word</div>
+            <div className="btn btn-info mx-3" onClick={handleCopy}>Copy Text</div>
+            <div className="btn btn-info mx-3" onClick={handleClearClick}>Clear Text</div>
+            <div className="btn btn-info mx-3" onClick={handleExtraSpaces}>Remove Extra Space</div>
+            {/* <div className="btn btn-dark mx-3" onClick={handlePaste}>Paste Text</div> */}
+
         </div>
-        <div className="container my-3">
+        <div className="container my-3 " style={{color:props.mode ==="dark"? "rgb(252 240 240)" :"#212529"}}>
             <h3>Your text summary:</h3>
             <p>{text.split(" ").length} words and {text.length} characters.</p>
-            <p>{text.split(" ").length * 0.008}min read</p>
-            <h3>Previw:</h3>
-            <p>{text}</p>
+            <p>{text.split(" ").length * 0.008} min read</p>
+            <h3>Preview:</h3>
+            <p>{text.length > 0 ? text : "Enter something in the above box to preview here!"}</p>
         </div>
         </>
     )
